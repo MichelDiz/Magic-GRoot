39c9f644e972d5eb911d0f236fa5c273de177def
Michel Diz <MichelDiz@users.noreply.github.com>
2025-03-01 19:20:39 -0300

    adiciona suporte a contexto na função ScanForScripts para gerenciamento de cancelamento

--- internal/scanner/scan.go
+++ internal/scanner/scan.go
@@ -1,6 +1,7 @@
 package scanner
 
 import (
+	"context"
 	"encoding/json"
 	"mgr/internal/config"
 	"os"
@@ -13,34 +14,41 @@ type PackageJSON struct {
 	Scripts map[string]string `json:"scripts"`
 }
 
-func ScanForScripts(rootPath string) map[string]map[string]string {
+func ScanForScripts(ctx context.Context, rootPath string) (map[string]map[string]string, error) {
 	projects := make(map[string]map[string]string)
-	_ = filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
 
-		if info.IsDir() && strings.HasSuffix(path, "node_modules") {
-			return filepath.SkipDir
-		}
-
-		if info.Name() == "package.json" {
-			packageData, err := os.ReadFile(path)
+	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
 			if err != nil {
 				return err
 			}
 
-			var pkg PackageJSON
-			if err := json.Unmarshal(packageData, &pkg); err != nil {
-				return err
+			if info.IsDir() && strings.HasSuffix(path, "node_modules") {
+				return filepath.SkipDir
 			}
 
-			projectPath := filepath.Dir(path)
-			projects[projectPath] = pkg.Scripts
+			if info.Name() == "package.json" {
+				packageData, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
-			config.SaveProjectToDB(projectPath, pkg.Name, pkg.Scripts)
+				var pkg PackageJSON
+				if err := json.Unmarshal(packageData, &pkg); err != nil {
+					return err
+				}
+
+				projectPath := filepath.Dir(path)
+				projects[projectPath] = pkg.Scripts
+
+				config.SaveProjectToDB(projectPath, pkg.Name, pkg.Scripts)
+			}
 		}
 		return nil
 	})
-	return projects
+
+	return projects, err
 }
