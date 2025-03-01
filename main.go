1bfe641b851afdca4b7242764b6bda121260a66b
Michel Diz <MichelDiz@users.noreply.github.com>
2025-03-01 19:19:10 -0300

    adiciona suporte a cancelamento e tratamento de sinal para encerramento gracioso da aplicação

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"mgr/cmd"
 	"mgr/internal/config"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"mgr/internal/logger"
 
 	"github.com/spf13/cobra"
 )
 
-func execute() {
+func execute(ctx context.Context) error {
 
 	var RootCmd = &cobra.Command{
 		Use:   "mgr",
@@ -23,13 +29,29 @@ func execute() {
 	RootCmd.AddCommand(cmd.GoCmd())
 	RootCmd.AddCommand(cmd.SetRunnerCmd())
 
-	cobra.CheckErr(RootCmd.Execute())
+	return RootCmd.ExecuteContext(ctx)
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		logger.Info.Println("Shutting down gracefully...")
+		cancel()
+	}()
+
 	config.InitDB()
 	config.InitI18n()
 	lang := config.GetLanguage()
 	config.SetConfig("language", lang)
-	execute()
+
+	if err := execute(ctx); err != nil {
+		logger.Error.Printf("Application error: %v\n", err)
+		os.Exit(1)
+	}
 }
